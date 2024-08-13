4a7f0b623203105afe01612f6b96a74c3d8d7eb2
ORyomei <o.ryomei1020@gmail.com>
2024-08-13 15:16:11 +0900

    設定ファイルを追加

---
+++ src/config.hpp
@@ -0,0 +1,20 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define SPI_2_MOSI_PIN 11
+#define SPI_2_MISO_PIN 13
+#define SPI_2_SCLK_PIN 12
+
+#define BMI160_SPI_HOST SPI2_HOST
+#define BMI160_SPI_CS_PIN 14 // TODO change
+
+#define MCP3208_SPI_HOST SPI2_HOST
+#define MCP3208_SPI_CS_PIN 10 // TODO change
+
+#define ADS8688_SPI_HOST SPI2_HOST
+#define ADS8688_SPI_CS_PIN 10 // TODO change
+
+#define NEO_M8U_SPI_HOST SPI3_HOST
+#define NEO_M8U_SPI_CS_PIN 15 // TODO change
+
+#endif
